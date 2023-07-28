b33a798f91b50139139febe2ee35da73078f0084
fadly <fadlyakbar444@gmail.com>
2023-07-28 08:52:36 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\kategori;
+use App\Models\produk;
+use App\Models\user;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        kategori::create([
+            'nama' => 'laptop gaming'
+        ]);
+        produk::create([
+            'nama' => 'ROG',
+            'kategori_id' => '1',
+            'harga' => '30000000',
+            'foto' => 'img/rog.jpg'
+        ]);
+        user::create([
+            'nama' => 'Fadly',
+            'email' => 'lapulapu@gmail.com',
+            'password' =>bcrypt('rajasaya2'),
+            'role' => 'admin'
+        ]);
+        user::create([
+            'nama' => 'customer',
+            'email' => 'lapilapi@gmail.com',
+            'password' => 'rajasaya1',
+            'role' => 'user'
+        ]);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>mencoba</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+                    <img src="https://4.bp.blogspot.com/-t_Zwx276KNI/U1Z9LG0f_ZI/AAAAAAAAGus/XwpyJwdmt-w/s1600/gambar+ikan+mas.jpg" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h2 class="card-tittle">haloo</h2>
+                        <p class="card-text">halooo</p>
+                        <a href="" class="btn btn-primary">Meluncur</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img src="https://4.bp.blogspot.com/-t_Zwx276KNI/U1Z9LG0f_ZI/AAAAAAAAGus/XwpyJwdmt-w/s1600/gambar+ikan+mas.jpg" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h2 class="card-tittle">haloo</h2>
+                        <p class="card-text">halooo</p>
+                        <a href="" class="btn btn-primary">Meluncur</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img src="https://4.bp.blogspot.com/-t_Zwx276KNI/U1Z9LG0f_ZI/AAAAAAAAGus/XwpyJwdmt-w/s1600/gambar+ikan+mas.jpg" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h2 class="card-tittle">haloo</h2>
+                        <p class="card-text">halooo</p>
+                        <a href="" class="btn btn-primary">Meluncur</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
